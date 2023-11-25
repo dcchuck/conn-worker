90d18d9ba9fe1356ca77eecdc0f796993087020e
Chuck Danielsson <chuck.danielsson@gmail.com>
2023-11-25 09:49:31 -0500

    update screen with data from worker

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 const worker = new SharedWorker(new URL('./worker.ts', import.meta.url))
 
-worker.port.onmessage = event => {
-  console.log('Received Worker onmessage', event)
-}
-
 worker.port.start()
-console.log(worker)
-
-const socketConnection = new WebSocket('ws://localhost:8765')
 
 const messageCountDiv = document.getElementById('message-count')
 const connectionCountDiv = document.getElementById('connection-count')
@@ -15,8 +8,7 @@ const connectionCountDiv = document.getElementById('connection-count')
 if (messageCountDiv === null) throw Error('Cannot find messageCountDiv')
 if (connectionCountDiv === null) throw Error('Cannot find connectionCountDiv')
 
-
-socketConnection.onmessage = message => {
+worker.port.onmessage = message => {
   const messageAsJson = JSON.parse(message.data)
   messageCountDiv.innerText = messageAsJson.messageCount
   connectionCountDiv.innerText = messageAsJson.connectionCount
